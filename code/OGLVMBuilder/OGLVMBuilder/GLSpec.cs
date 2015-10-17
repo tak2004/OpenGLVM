5af9d8cce3bfe66f0415cbebae7a6c062304160a
TAK2004 <thomas@ptscientists.com>
2015-10-17 14:25:00 +0200

    -added OGLVMBuilder -uml diagram of vm byte code intermediate format

---
+++ code/OGLVMBuilder/OGLVMBuilder/GLSpec.cs
@@ -0,0 +1,328 @@
+﻿/*
+ * Copyright (c) 2013, Nick Gravelyn.
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ *    1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 
+ *    2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 
+ *    3. This notice may not be removed or altered from any source
+ *    distribution.
+ * 
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace GLCSGen
+{
+    public class GLSpec
+    {
+        [XmlElement("Comment")]
+        public string HeaderComment;
+
+        [XmlArrayItem("Version")]
+        public List<GLVersion> Versions = new List<GLVersion>();
+
+        public static GLSpec FromFile(string xmlFile)
+        {
+            XDocument doc = XDocument.Load(xmlFile);
+            GLSpec spec = new GLSpec();
+            spec.HeaderComment = doc.Root.Element("comment").Value;
+
+            foreach (var e in doc.Root.Elements("feature"))
+            {
+                // OpenGL is super special because it simply includes all enums and commands
+                if (e.Attribute("api").Value == "gl" && e.Attribute("number").Value == "1.0")
+                {
+                    AddVersion1(spec, e);
+                }
+                // OpenGL 3.0 and up have Compatibility profile and Core profile
+                else if (e.Attribute("api").Value == "gl" && int.Parse(e.Attribute("number").Value[0].ToString()) >= 3)
+                {
+                    AddVersion(spec, e, "Compatibility");
+                    AddVersion(spec, e, "Core");
+                }
+                // OpenGL pre-3.0 and OpenGL ES don't have profiles
+                else
+                {
+                    AddVersion(spec, e, string.Empty);
+                }
+            }
+
+            return spec;
+        }
+
+        /// <summary>
+        /// Super special OpenGL 1 version handler
+        /// </summary>
+        private static void AddVersion1(GLSpec spec, XElement versionElem)
+        {
+            var version = new GLVersion
+            {
+                Api = "GL",
+                Number = "1.0",
+                Profile = string.Empty,
+                Name = "GL10"
+            };
+
+            // First we process commands like any other version
+            foreach (var require in versionElem.Elements("require"))
+            {
+                foreach (var commandElem in require.Elements("command"))
+                {
+                    var command = new GLCommand { Name = commandElem.Attribute("name").Value };
+                    command.Initialize(commandElem.Document);
+                    version.Commands.Add(command);
+                }
+            }
+
+            // Then we go through all included commands and use all the groups on any parameters
+            // to include all the enums we need from the groups section
+            foreach (var group in (from c in version.Commands from p in c.Parameters select p.Group).Distinct())
+            {
+                if (string.IsNullOrEmpty(group))
+                {
+                    continue;
+                }
+
+                foreach (var enumElem in versionElem.Document.Root.Element("groups").Elements("group").Where(e => e.Attribute("name").Value == group).Elements("enum"))
+                {
+                    if (!version.Enums.ContainsKey(enumElem.Attribute("name").Value))
+                    {
+                        string enumName = enumElem.Attribute("name").Value;
+                        version.Enums.Add(enumName, GetEnumValue(versionElem, enumName));
+                    }
+                }
+            }
+
+            spec.Versions.Add(version);
+        }
+
+        /// <summary>
+        /// Creates a given version.
+        /// </summary>
+        private static void AddVersion(GLSpec spec, XElement versionElem, string profile)
+        {
+            var version = new GLVersion
+            {
+                Api = versionElem.Attribute("api").Value.ToUpper(),
+                Number = versionElem.Attribute("number").Value,
+                Profile = profile
+            };
+
+            if (version.Api != "GL")
+            {
+                version.Api = "GLES";
+            }
+
+            version.Name = version.Api + version.Number.Replace(".", "");
+            if (!string.IsNullOrEmpty(profile))
+            {
+                version.Name += "" + profile;
+            }
+
+            // Add all enums and commands from previous version
+            for (int i = spec.Versions.Count - 1; i >= 0; i--)
+            {
+                var previousVersion = spec.Versions[i];
+                if (previousVersion.Api == version.Api &&
+                    (previousVersion.Profile == string.Empty || previousVersion.Profile == version.Profile))
+                {
+                    foreach (var pair in previousVersion.Enums)
+                    {
+                        version.Enums.Add(pair.Key, pair.Value);
+                    }
+
+                    foreach (var command in previousVersion.Commands)
+                    {
+                        version.Commands.Add(command.Clone());
+                    }
+
+                    // only handle previous one version of same API and profile since it will have folded
+                    // all enums/commands in turn
+                    break;
+                }
+            }
+
+            // Include all new enums and commands
+            foreach (var require in versionElem.Elements("require"))
+            {
+                foreach (var enumElem in require.Elements("enum"))
+                {
+                    if (!version.Enums.ContainsKey(enumElem.Attribute("name").Value))
+                    {
+                        string enumName = enumElem.Attribute("name").Value;
+                        version.Enums.Add(enumName, GetEnumValue(versionElem, enumName));
+                    }
+                }
+
+                foreach (var commandElem in require.Elements("command"))
+                {
+                    var command = new GLCommand { Name = commandElem.Attribute("name").Value };
+                    if (version.Commands.Find(c => c.Name == command.Name) != null)
+                    {
+                        continue;
+                    }
+
+                    command.Initialize(commandElem.Document);
+                    version.Commands.Add(command);
+                }
+            }
+
+            // Remove any enums and commands
+            foreach (var remove in versionElem.Elements("remove"))
+            {
+                if (remove.Attribute("profile") == null || 
+                    remove.Attribute("profile").Value.ToLower() == profile.ToLower())
+                {
+                    foreach (var en in remove.Elements("enum"))
+                    {
+                        version.Enums.Remove(en.Attribute("name").Value);
+                    }
+                    foreach (var en in remove.Elements("command"))
+                    {
+                        version.Commands.RemoveAll(c => c.Name == en.Attribute("name").Value);
+                    }
+                }
+            }
+
+            spec.Versions.Add(version);
+        }
+
+        private static string GetEnumValue(XElement versionElem, string enumName)
+        {
+            string enumValue = string.Empty;
+
+            foreach (var enumsElements in versionElem.Document.Root.Elements("enums"))
+            {
+                foreach (var enumElement in enumsElements.Elements("enum"))
+                {
+                    if (enumElement.Attribute("name").Value == enumName &&
+                        (enumElement.Attribute("api") == null || enumElement.Attribute("api").Value == versionElem.Attribute("api").Value))
+                    {
+                        enumValue = enumElement.Attribute("value").Value;
+                        break;
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(enumValue))
+                {
+                    break;
+                }
+            }
+            return enumValue;
+        }
+    }
+    
+    public class GLVersion
+    {
+        [XmlAttribute]
+        public string Api;
+
+        [XmlAttribute]
+        public string Name;
+
+        [XmlAttribute]
+        public string Profile;
+
+        [XmlAttribute]
+        public string Number;
+
+        public EnumDictionary Enums = new EnumDictionary();
+
+        [XmlArrayItem("Command")]
+        public List<GLCommand> Commands = new List<GLCommand>();
+    }
+
+    public class GLParameter
+    {
+        [XmlAttribute]
+        public string Group;
+
+        [XmlAttribute]
+        public string Type;
+
+        [XmlAttribute]
+        public string Name;
+    }
+
+    public class GLCommand
+    {
+        [XmlAttribute]
+        public string Name;
+
+        [XmlAttribute]
+        public string ReturnType;
+
+        [XmlArrayItem("Parameter")]
+        public List<GLParameter> Parameters = new List<GLParameter>();
+
+        public GLCommand Clone()
+        {
+            return new GLCommand
+            {
+                Name = Name,
+                ReturnType = ReturnType,
+                Parameters = (from p in Parameters select new GLParameter { Name = p.Name, Type = p.Type, Group = p.Group }).ToList()
+            };
+        }
+
+        public void Initialize(XDocument specDoc)
+        {
+            var commandElem = (from elem in specDoc.Root.Element("commands").Elements("command")
+                               where elem.Element("proto").Element("name").Value == Name
+                               select elem).First();
+
+            ReturnType = commandElem.Element("proto").Value.Replace(Name, string.Empty).Trim();
+
+            foreach (var p in commandElem.Elements("param"))
+            {
+                var param = new GLParameter { Name = p.Element("name").Value };
+                param.Type = p.Value.Substring(0, p.Value.LastIndexOf(param.Name)).Trim();
+                if (p.Attribute("group") != null)
+                {
+                    param.Group = p.Attribute("group").Value;
+                }
+                Parameters.Add(param);
+            }
+        }
+    }
+
+    public class EnumDictionary : Dictionary<string, string>, IXmlSerializable
+    {
+        System.Xml.Schema.XmlSchema IXmlSerializable.GetSchema()
+        {
+            return null;
+        }
+
+        void IXmlSerializable.ReadXml(System.Xml.XmlReader reader)
+        {
+            // we don't care about parsing our format back
+        }
+
+        void IXmlSerializable.WriteXml(System.Xml.XmlWriter writer)
+        {
+            foreach (var entry in this)
+            {
+                writer.WriteStartElement("Enum");
+                writer.WriteAttributeString("Name", entry.Key);
+                writer.WriteAttributeString("Value", entry.Value);
+                writer.WriteEndElement();
+            }
+        }
+    }
+}
---
+++ code/OGLVMBuilder/OGLVMBuilder/MainWindow.xaml.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace OGLVMBuilder
+{
+    public class State
+    {
+        public string Name { get; private set; }
+        public ObservableCollection<Command> Commands { get; private set; }
+        public State(string name) { 
+            this.Name = name;
+            Commands = new ObservableCollection<Command>();
+        }
+    }    
+
+    public class Command
+    {
+        public string Name { get; set; }
+        public Command(string name)
+        { 
+            this.Name = name;
+        }
+    }    
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Point _dragStartPoint;
+        private bool dataChanged = false;
+        
+        private T FindVisualParent<T>(DependencyObject child)
+            where T:DependencyObject
+        {
+            var parentObject = VisualTreeHelper.GetParent(child);
+            if (parentObject == null)
+                return null;
+            T parent = parentObject as T;
+            if (parent != null)
+                return parent;
+            return FindVisualParent<T>(parentObject);
+        }
+
+        private ObservableCollection<State> code = new ObservableCollection<State>() { };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            code.Add(new State("initialize"));
+            code.Add(new State("shutdown"));
+            code[0].Commands.Add(new Command("glBegin"));
+            code[0].Commands.Add(new Command("glEnd"));
+            code[1].Commands.Add(new Command("glBegin"));
+            code[1].Commands.Add(new Command("glEnd"));
+
+            DataContext = new
+            {
+                Code = code
+            };
+
+            LoadSpecs();
+        }
+
+        private void ListBoxItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _dragStartPoint = e.GetPosition(null);
+        }
+
+        private void ListBoxItem_Drop(object sender, DragEventArgs e)
+        {
+             if (sender is TextBlock)
+             {
+                 var source = e.Data.GetData(typeof(Command)) as Command;
+                 var target = ((TextBlock)(sender)).DataContext as Command;
+
+//                  int sourceIndex = -1;
+//                  foreach(var item in CodeTree.Items)
+//                     item.IndexOf(source);
+//                  int targetIndex = CodeTree.Items.IndexOf(target);
+ 
+//                 if (sourceIndex == -1)
+//                {
+//                     if (target.AllowParent)
+//                         _items.Insert(targetIndex + 1, new VMItem(e.Data.GetData(typeof(string)) as string, true));
+//                     else
+//                         _items.Insert(targetIndex + 1, new VMItem(e.Data.GetData(typeof(string)) as string,true));
+/*                }*/
+//                else
+/*                {*/
+               //     Move(source, sourceIndex, targetIndex);
+//                 }
+             }
+        }
+
+//         private void Move(VMItem source, int sourceIndex, int targetIndex)
+//         {
+//             if (sourceIndex < targetIndex)
+//             {
+//                 _items.Insert(targetIndex + 1, source);
+//                 _items.RemoveAt(sourceIndex);
+//             }
+//             else
+//             {
+//                 int removeIndex = sourceIndex + 1;
+//                 if (_items.Count + 1 > removeIndex)
+//                 {
+//                     _items.Insert(targetIndex, source);
+//                     _items.RemoveAt(removeIndex);
+//                 }
+//             }
+//         }
+
+        private void LoadSpecs()
+        {
+            Hashtable knownCommands = null;
+            if (!System.IO.File.Exists("dump.bin"))
+            {
+
+                var specs = GLCSGen.GLSpec.FromFile("gl.xml");
+
+                knownCommands = new Hashtable();
+
+                foreach (var version in specs.Versions)
+                {
+                    foreach (var command in version.Commands)
+                    {
+                        if (!knownCommands.ContainsKey(command.Name))
+                        {
+                            knownCommands.Add(command.Name, command.Name);
+                        }
+                    }
+                }
+
+                System.IO.FileStream fs = new System.IO.FileStream("dump.bin", System.IO.FileMode.Create);
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(fs, knownCommands);
+
+            }
+            else
+            {
+                System.IO.FileStream fs = new System.IO.FileStream("dump.bin", System.IO.FileMode.Open);
+                BinaryFormatter formatter = new BinaryFormatter();
+                knownCommands = (Hashtable)formatter.Deserialize(fs);
+            }
+
+            foreach(DictionaryEntry command in knownCommands)
+            {
+                CommandList.Items.Add(command.Value);
+            }
+        }
+
+        private void AppExit_Click(object sender, RoutedEventArgs e)
+        {
+            DataCheck();
+            Application.Current.Shutdown();
+        }
+
+        private void NewCode_Click(object sender, RoutedEventArgs e)
+        {
+            DataCheck();
+            code.Clear();
+        }
+
+        protected void SaveData(string filename)
+        {
+            dataChanged = false;
+        }
+
+        protected void LoadData(string filename)
+        {
+            dataChanged = false;
+        }
+
+        protected void DataCheck()
+        {
+            if (dataChanged == true)
+            {
+                MessageBoxButton btnMessageBox = MessageBoxButton.YesNo;
+                MessageBoxImage icnMessageBox = MessageBoxImage.Warning;
+
+                MessageBoxResult rsltMessageBox = MessageBox.Show("Do you want to save the current data ?",
+                    "Possible data loss", btnMessageBox, icnMessageBox);
+
+                if(rsltMessageBox ==MessageBoxResult.Yes)
+                {
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    if (saveFileDialog.ShowDialog() == true)
+                        SaveData(saveFileDialog.FileName);
+                }
+            }
+        }
+
+        private void OpenCode_Click(object sender, RoutedEventArgs e)
+        {
+            DataCheck();
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                LoadData(openFileDialog.FileName);
+        }
+
+        private void SaveCode_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+                SaveData(saveFileDialog.FileName);
+        }
+
+        private void CommandList_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ListBox parent = (ListBox)sender;
+            TextBlock draggedItem = e.OriginalSource as TextBlock;
+
+            if (draggedItem != null)
+            {
+                DragDrop.DoDragDrop(parent, draggedItem.Text, DragDropEffects.Move);
+            }
+        }
+
+        private void TreeView_Drop(object sender, DragEventArgs e)
+        {
+            TextBlock parent = (TextBlock)sender;
+            object data = e.Data.GetData(typeof(string));
+            if (data != null && parent.DataContext is State)
+            {
+                State state = (State)parent.DataContext;
+                state.Commands.Add(new Command(data as string));
+                dataChanged = true;
+            }
+        }
+
+        private void TreeView_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            Point point = e.GetPosition(null);
+            Vector diff = _dragStartPoint - point;
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                    Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+                TextBlock lbi = (TextBlock)e.OriginalSource;
+                if (lbi != null)
+                {
+                    DragDrop.DoDragDrop(lbi, lbi.DataContext, DragDropEffects.Move);
+                }
+            }
+        }
+        
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SectionDialog dlg = new SectionDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                code.Add(new State(dlg.Statename));
+                dataChanged = true;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DataCheck();
+        }
+
+        private void DeleteCommand_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            ContextMenu menu = item.CommandParameter as ContextMenu;
+            if (menu.PlacementTarget is TextBlock)
+            {
+                TextBlock cmdComponent = menu.PlacementTarget as TextBlock;
+                Command cmd = cmdComponent.DataContext as Command;
+                foreach(var state in code)
+                {
+                    if (state.Commands.Remove(cmd))
+                        break;
+                }
+                dataChanged = true;
+            }
+        }
+
+        private void DeleteState_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            ContextMenu menu = item.CommandParameter as ContextMenu;
+            if (menu.PlacementTarget is TextBlock)
+            {
+                TextBlock stateComponent = menu.PlacementTarget as TextBlock;
+                State state = stateComponent.DataContext as State;
+                code.Remove(state);
+                dataChanged = true;
+            }
+        }
+    }
+}
